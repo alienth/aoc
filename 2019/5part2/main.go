2d5bf4bbc9d1d360f92720092e3287ef9cad7a9d
Jason Harvey <alienth@gmail.com>
2019-12-16 12:11:54 -0900

    5part2.

---
+++ 2019/5part2/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const OpcodeAdd = 1
+const OpcodeMultiply = 2
+const OpcodeInput = 3
+const OpcodeOutput = 4
+const OpcodeJumpIfTrue = 5
+const OpcodeJumpIfFalse = 6
+const OpcodeLessThan = 7
+const OpcodeEquals = 8
+const OpcodeEnd = 99
+
+type paramMode int
+
+const (
+	paramModePosition paramMode = iota
+	paramModeImmediate
+)
+
+func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	input := make([]int, 0)
+	s := bufio.NewScanner(f)
+	s.Scan()
+	line := s.Text()
+	items := strings.Split(line, ",")
+	for _, item := range items {
+		i, err := strconv.Atoi(item)
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = append(input, i)
+	}
+	execute(input)
+}
+
+func execute(input []int) int {
+	last := len(input) - 1
+
+	for pos := 0; pos <= last; {
+		// fmt.Println("At position ", pos)
+		switch instruction := processInstruction(input[pos]); instruction.opcode {
+		case OpcodeAdd, OpcodeMultiply, OpcodeLessThan, OpcodeEquals: // 3 params
+			math(input, pos, instruction)
+			pos += 4
+		case OpcodeJumpIfTrue: // 2 params
+			fmt.Printf("Instruction: %v\n", input[pos:pos+3])
+			paramValues := getParamValues(input, instruction.paramModes, input[pos+1:pos+3])
+			if paramValues[0] != 0 {
+				pos = paramValues[1]
+			} else {
+				pos += 3
+			}
+			fmt.Printf("Jumping to: %v\n", pos)
+		case OpcodeJumpIfFalse: // 2 params
+			fmt.Printf("Instruction: %v\n", input[pos:pos+3])
+			paramValues := getParamValues(input, instruction.paramModes, input[pos+1:pos+3])
+			if paramValues[0] == 0 {
+				pos = paramValues[1]
+			} else {
+				pos += 3
+			}
+			fmt.Printf("Jumping to: %v\n", pos)
+		case OpcodeInput: // 1 param
+			fmt.Printf("Instruction: %v\n", input[pos:pos+2])
+			store := input[pos+1]
+			input[store] = 5 // Our "input"
+			pos += 2
+		case OpcodeOutput: // 1 param
+			fmt.Printf("Instruction: %v\n", input[pos:pos+2])
+			paramValues := getParamValues(input, instruction.paramModes, []int{input[pos+1]})
+			fmt.Println("Output: ", paramValues[0])
+			pos += 2
+		case OpcodeEnd:
+			return input[0]
+		default:
+			log.Fatalf("Unknown opcode %d on instruction %d.\n", instruction.opcode, input[pos])
+		}
+	}
+	return 0
+}
+
+func math(input []int, pos int, ins instruction) {
+	fmt.Printf("Instruction: %v\n", input[pos:pos+4])
+	params := input[pos+1 : pos+3]
+	paramValues := getParamValues(input, ins.paramModes, params)
+	store := input[pos+3]
+	result := 0
+
+	switch ins.opcode {
+	case OpcodeAdd:
+		result = add(paramValues)
+		fmt.Printf("Adding %v and storing sum %d at address %d.\n", paramValues, result, store)
+	case OpcodeMultiply:
+		result = multiply(paramValues)
+		fmt.Printf("Multiplying %v and storing product %d at address %d.\n", paramValues, result, store)
+	case OpcodeLessThan:
+		if paramValues[0] < paramValues[1] {
+			result = 1
+		} else {
+			result = 0
+		}
+		fmt.Printf("Less than on %v and storing result %d at address %d.\n", paramValues, result, store)
+	case OpcodeEquals:
+		if paramValues[0] == paramValues[1] {
+			result = 1
+		} else {
+			result = 0
+		}
+		fmt.Printf("Equals on %v and storing result %d at address %d.\n", paramValues, result, store)
+	}
+
+	input[store] = result
+}
+
+func add(operands []int) int {
+	sum := 0
+	for _, o := range operands {
+		sum += o
+	}
+	return sum
+}
+
+func multiply(operands []int) int {
+	return operands[0] * operands[1]
+}
+
+type instruction struct {
+	opcode     int
+	paramModes []paramMode
+}
+
+func getParamValues(input []int, modes []paramMode, params []int) []int {
+	// fmt.Println("Modes ", modes)
+	// fmt.Println("Params ", params)
+	values := make([]int, len(params))
+
+	for i, p := range params {
+		mode := paramModePosition
+		if len(modes) > i {
+			mode = modes[i]
+		}
+		switch mode {
+		case paramModePosition:
+			values[i] = input[p]
+		case paramModeImmediate:
+			values[i] = p
+		default:
+			log.Fatal("Unknown param mode: ", mode)
+		}
+	}
+	// fmt.Println("Values ", values)
+	return values
+}
+
+func processInstruction(i int) instruction {
+	digits := getDigits(i)
+	reverseSlice(digits)
+
+	var ins instruction
+	ins.paramModes = make([]paramMode, 0)
+	ins.opcode = digits[0]
+	if len(digits) > 1 {
+		ins.opcode += digits[1] * 10
+	}
+
+	for i := 2; i < len(digits); i++ {
+		ins.paramModes = append(ins.paramModes, paramMode(digits[i]))
+	}
+
+	return ins
+}
+
+func reverseSlice(a []int) []int {
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
+	return a
+}
+
+// Takes an integer and splits it into its composite digits.
+func getDigits(i int) []int {
+	digitChars := strings.Split(fmt.Sprintf("%d", i), "")
+	digits := make([]int, len(digitChars))
+
+	for i, d := range digitChars {
+		digits[i], _ = strconv.Atoi(d)
+	}
+	return digits
+}
